7b06568cc135a321fd8774f10f0f3e3092fa6a7c
Mat Gargano <mgargano@gmail.com>
2015-11-10 22:11:50 -0500

    fix bug

--- src/WPize.php
+++ src/WPize.php
@@ -1,126 +1,58 @@
 <?php
 
-namespace WPize;
+namespace WPize\Utils;
 
-use \WPize\Consumers;
-use WPize\Utils\Utils;
+class Utils {
 
-class WPize
-{
+    public static function recursivelyRemoveDirectory( $dir ) {
+        if ( is_dir( $dir ) ) {
+            $objects = scandir( $dir );
+            foreach ( $objects as $object ) {
+                if ( $object != "." && $object != ".." ) {
+                    if ( filetype( $dir . "/" . $object ) == "dir" ) {
+                        self::recursivelyRemoveDirectory( $dir . "/" . $object );
+                    } else {
+                        unlink( $dir . "/" . $object );
+                    }
+                }
+            }
+            reset( $objects );
+            rmdir( $dir );
+        }
+    }
 
-    private $config;
-    private $realBase;
-    private $piece;
-    private $currentPostProcess;
+    public static function createTempDir() {
 
-    public function __construct(Array $config = array())
+        $tempfile = tempnam( sys_get_temp_dir(), '' );
+        if ( file_exists( $tempfile ) ) {
+            unlink( $tempfile );
+        }
+        mkdir( $tempfile );
+        if ( is_dir( $tempfile ) ) {
+            return $tempfile;
+        }
+
+        return false;
+    }
+
+    public static function recursivelyCopy($src, $dst, Array $exceptions = array() )
     {
-        if (!$config) {
-            throw new \Exception('Please pass in a configuration array');
+        $dir = opendir($src);
+        if (!is_dir($dst)) {
+            mkdir($dst);
         }
-        $this->config = $config;
+        while (false !== ($file = readdir($dir))) {
+            if ( ( $file != '.' ) && ( $file != '..' ) && ( ! is_array( $exceptions ) || ( isset( $exceptions['directories'] ) && is_array($exceptions['directories']) && ! in_array( $file, $exceptions['directories'] ) ) ) ) {
+                if (is_dir($src . '/' . $file)) {
+                    self::recursivelyCopy( $src . '/' . $file, $dst . '/' . $file, $exceptions );
+                } elseif ( is_array( $exceptions ) && isset( $exceptions['files'] ) && is_array($exceptions['files']) && ! in_array( $file, $exceptions['files'] ) ) {
+
+                    copy($src . '/' . $file, $dst . '/' . $file);
+                }
+            }
+        }
+        closedir($dir);
     }
-
-    public function process()
-    {
-
-
-        if (!isset($this->config['tempBase']) || !$this->config['tempBase']) {
-            $this->realBase = Utils::createTempDir();
-            chdir($this->realBase);
-            mkdir('wpize');
-            $this->realBase .= '/wpize';
-        } else {
-            $this->realBase = $this->config['tempBase'];
-        }
-	    Utils::recursivelyRemoveDirectory($this->realBase);
-        if (!$this->realBase) {
-            throw new \Exception('Issue creating temporary directory');
-        }
-        if (is_dir($this->realBase)) {
-            $this->recursivelyRemoveBase();
-        }
-
-
-        foreach ($this->config['pieces'] as $piece) {
-
-            $this->piece = $piece;
-            $this->processPiece();
-
-
-        }
-        $this->postProcess();
-
-
-    }
-
-    public function postProcess()
-    {
-
-        if (!isset($this->config['post']) || !is_array($this->config['post'])) {
-            return;
-        }
-
-        foreach($this->config['post'] as $post) {
-
-            $this->currentPostProcess = $post;
-            $this->processPostStep();
-
-
-        }
-
-    }
-
-    public function processPostStep(){
-
-
-        $class = 'WPize\\Post_Process\\' . $this->currentPostProcess['type'];
-
-        if (class_exists($class)) {
-
-            /**
-             * @var \WPize\Post_Process\Post_Process_Base $handle
-             */
-
-            $handle = new $class($this->currentPostProcess, $this->realBase);
-            $handle->handle();
-            $handle = null; //destruct the object to clear the directory
-        }
-
-    }
-
-    public function processPiece()
-    {
-
-        $type = 'Shell';
-        if (isset($this->piece['retrieve']['type'])) {
-            $type = $this->piece['retrieve']['type'];
-        }
-
-        $class = 'WPize\\Consumers\\' . $type;
-
-        if (class_exists($class)) {
-
-            /**
-             * @var \WPize\Consumers\Consumer_Base $handle
-             */
-
-            $handle = new $class($this->piece, $this->realBase);
-            $handle->handle();
-            $handle = null; //destruct the object to clear the directory
-        }
-
-    }
-
-    public function recursivelyRemoveBase()
-    {
-
-        Utils::recursivelyRemoveDirectory($this->realBase);
-    }
-
 
 
 }
-
-
-
